3236bf042fd51afd45db48267bcccf65499cf207
maczi01 <mateusz.w.twardy@gmail.com>
2023-08-18 23:04:56 +0100

    Added listener in router

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -30,5 +30,12 @@ const router = async () => {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+
+    document.body.addEventListener("click", e => {
+        if(e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    })
     router();
 })
